a6a9758f5ab5735f65dd76d228b62388e7a3a988
eymaster <61962873+eymaster@users.noreply.github.com>
2025-07-28 11:42:54 +0800

    Create script.js

---
+++ templates/script.js
@@ -0,0 +1,19 @@
+document.getElementById('homeLink').addEventListener('click', () => {
+    alert('Navigating to Home');
+});
+
+document.getElementById('profileLink').addEventListener('click', () => {
+    alert('Navigating to Profile');
+});
+
+document.getElementById('settingsLink').addEventListener('click', () => {
+    alert('Navigating to Settings');
+});
+
+document.getElementById('logoutLink').addEventListener('click', () => {
+    alert('Logging out...');
+});
+
+document.querySelector('.profile-btn').addEventListener('click', () => {
+    alert('Viewing Profile');
+});
